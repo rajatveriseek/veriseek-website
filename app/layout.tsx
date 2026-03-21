b9ea7f4e84c6763c32899860d862b1ef8a9f6ae9
Rishabh Didwania MAQ Software <rishabhd@maqsoftware.com>
2026-03-21 13:03:36 +0530

    Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,18 @@ export default function RootLayout({
         <link rel="preconnect" href="https://maps.googleapis.com" />
         <link rel="preconnect" href="https://maps.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
+
+        {/* ✅ Google Tag Manager Script */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5C3LMFWR');
+          `}
+        </Script>
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -73,7 +85,25 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable, dmSans.variable, playfairDisplay.variable)}>
+
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable,
+          dmSans.variable,
+          playfairDisplay.variable
+        )}
+      >
+        {/* ✅ Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5C3LMFWR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
@@ -88,13 +118,16 @@ export default function RootLayout({
             fbq('track', 'PageView');
           `}
         </Script>
+
         <AnalyticsWrapper ga_id={GOOGLE_ANALYTICS_ID} />
         <GlobalScrollReveal />
+
         <div className="relative flex min-h-screen flex-col">
           <Header />
           <main className="flex-1">{children}</main>
           <Footer />
         </div>
+
         <GlobalWhatsAppButton />
       </body>
     </html>
